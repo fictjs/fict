ebb9ba940ac29b057c1edb00f4801d0fa7732574
unadlib <unadlib@gmail.com>
2025-12-14 14:32:33 +0800

    chore(others): remove compiler-ts

--- packages/compiler-ts/packages/fict/src/jsx-dev-runtime.ts
+++
@@ -1,7 +0,0 @@
-/**
- * JSX dev runtime for Fict
- *
- * This module is used by the JSX transform in development mode.
- */
-
-export { jsx, jsxs, jsxDEV, Fragment, type JSX } from 'fict-runtime/jsx-runtime'
