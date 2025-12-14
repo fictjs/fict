d8138a12665a63391a6ba6fcfbd233cd012b581c
unadlib <unadlib@gmail.com>
2025-12-14 14:07:18 +0800

    test(others): refactor test

--- packages/eslint-plugin/__tests__/index.test.ts
+++ packages/eslint-plugin/__tests__/index.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest'
 
-import plugin from '..'
+import plugin from '../src/index'
 
 describe('eslint-plugin-fict', () => {
   it('exposes renamed rules', () => {
