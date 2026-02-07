735e55f9f194a9dbf74bed1e3b178d33afef57ab
unadlib <unadlib@gmail.com>
2026-02-07 14:46:36 +0800

    refactor(compiler): extract resumable component registration

--- packages/compiler/src/ir/codegen.ts
+++ packages/compiler/src/ir/codegen.ts
@@ -69,11 +69,11 @@ import {
   isLikelyTextExpression,
 } from './codegen-reactivity'
 import { findContainingRegion, regionInfoToMetadata } from './codegen-region-utils'
+import { registerResumableComponent } from './codegen-resumable-component'
 import {
   emitResumableEventBinding,
   type ResumableEventBindingOps,
 } from './codegen-resumable-events'
-import { genModuleUrlExpr } from './codegen-resumable-utils'
 import { collectRuntimeImports } from './codegen-runtime-imports'
 import { applySelectorHoist } from './codegen-selector-hoist'
 import {
@@ -3022,134 +3022,6 @@ function lowerIntrinsicElement(
 
   // Wrap in IIFE
   return t.callExpression(t.arrowFunctionExpression([], body), [])
-}
-
-function registerResumableComponent(componentName: string, ctx: CodegenContext): void {
-  if (!ctx.resumableEnabled) return
-  if (!ctx.hoistedResumableStatements || !ctx.resumableComponents) return
-  if (ctx.resumableComponents.has(componentName)) return
-
-  const { t } = ctx
-  const resumeExport = `__fict_r${ctx.resumableComponentCounter ?? 0}`
-  ctx.resumableComponentCounter = (ctx.resumableComponentCounter ?? 0) + 1
-
-  const scopeParam = t.identifier('scopeId')
-  const hostParam = t.identifier('host')
-  const snapshotId = t.identifier('snapshot')
-  const ctxId = t.identifier('ctx')
-
-  ctx.helpersUsed.add('getSSRScope')
-  ctx.helpersUsed.add('ensureScope')
-  ctx.helpersUsed.add('prepareContext')
-  ctx.helpersUsed.add('pushContext')
-  ctx.helpersUsed.add('popContext')
-  ctx.helpersUsed.add('hydrateComponent')
-  ctx.helpersUsed.add('qrl')
-
-  const snapshotDecl = t.variableDeclaration('const', [
-    t.variableDeclarator(
-      snapshotId,
-      t.callExpression(t.identifier(RUNTIME_ALIASES.getSSRScope), [scopeParam]),
-    ),
-  ])
-  const earlyReturn = t.ifStatement(t.unaryExpression('!', snapshotId), t.returnStatement())
-  const ensureCtxDecl = t.variableDeclaration('const', [
-    t.variableDeclarator(
-      ctxId,
-      t.callExpression(t.identifier(RUNTIME_ALIASES.ensureScope), [
-        scopeParam,
-        hostParam,
-        snapshotId,
-      ]),
-    ),
-  ])
-  // Prepare context so __fictPushContext will use it
-  const prepareCtx = t.expressionStatement(
-    t.callExpression(t.identifier(RUNTIME_ALIASES.prepareContext), [ctxId]),
-  )
-  // Push context onto ctxStack so __fictUseContext can find it
-  const pushCtx = t.expressionStatement(
-    t.callExpression(t.identifier(RUNTIME_ALIASES.pushContext), []),
-  )
-  // Use hydrateComponent which runs view INSIDE withHydration for proper DOM claiming
-  const hydrateCall = t.expressionStatement(
-    t.callExpression(t.identifier(RUNTIME_ALIASES.hydrateComponent), [
-      t.arrowFunctionExpression(
-        [],
-        t.callExpression(t.identifier(componentName), [
-          t.logicalExpression(
-            '||',
-            t.memberExpression(snapshotId, t.identifier('props')),
-            t.objectExpression([]),
-          ),
-        ]),
-      ),
-      hostParam,
-    ]),
-  )
-  const popCtx = t.expressionStatement(
-    t.callExpression(t.identifier(RUNTIME_ALIASES.popContext), []),
-  )
-
-  const resumeFnId = t.identifier(resumeExport)
-  const resumeFn = t.exportNamedDeclaration(
-    t.variableDeclaration('const', [
-      t.variableDeclarator(
-        resumeFnId,
-        t.arrowFunctionExpression(
-          [scopeParam, hostParam],
-          t.blockStatement([
-            snapshotDecl,
-            earlyReturn,
-            ensureCtxDecl,
-            t.tryStatement(
-              t.blockStatement([prepareCtx, pushCtx, hydrateCall]),
-              null,
-              t.blockStatement([popCtx]),
-            ),
-          ]),
-        ),
-      ),
-    ]),
-    [],
-  )
-
-  // Register resume function to prevent tree-shaking
-  // This creates a side effect that keeps the export alive
-  ctx.helpersUsed.add('registerResume')
-  const registerCall = t.expressionStatement(
-    t.callExpression(t.identifier(RUNTIME_ALIASES.registerResume), [
-      t.stringLiteral(resumeExport),
-      resumeFnId,
-    ]),
-  )
-
-  const metaId = t.identifier(`__fict_meta_${componentName}`)
-  const moduleUrlExpr = genModuleUrlExpr(ctx)
-  const typeKeyExpr = t.binaryExpression('+', t.stringLiteral(`${componentName}@`), moduleUrlExpr)
-  const resumeQrlExpr = t.callExpression(t.identifier(RUNTIME_ALIASES.qrl), [
-    genModuleUrlExpr(ctx),
-    t.stringLiteral(resumeExport),
-  ])
-  const metaDecl = t.variableDeclaration('const', [
-    t.variableDeclarator(
-      metaId,
-      t.objectExpression([
-        t.objectProperty(t.identifier('id'), typeKeyExpr),
-        t.objectProperty(t.identifier('resume'), resumeQrlExpr),
-      ]),
-    ),
-  ])
-  const assignMeta = t.expressionStatement(
-    t.assignmentExpression(
-      '=',
-      t.memberExpression(t.identifier(componentName), t.identifier('__fictMeta')),
-      metaId,
-    ),
-  )
-
-  ctx.hoistedResumableStatements.push(resumeFn, registerCall, metaDecl, assignMeta)
-  ctx.resumableComponents.set(componentName, { resumeExport, typeKey: componentName })
 }
 
 /**
