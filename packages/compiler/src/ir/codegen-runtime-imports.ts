e2bcab11e63c6d45178c3b5f86fe56d0ad0c0470
unadlib <unadlib@gmail.com>
2026-02-07 12:00:51 +0800

    refactor(compiler): extract runtime import discovery helpers

---
+++ packages/compiler/src/ir/codegen-runtime-imports.ts
@@ -0,0 +1,48 @@
+import type * as BabelCore from '@babel/core'
+
+import { RUNTIME_MODULE } from '../constants'
+
+export interface RuntimeImportCollection {
+  names: Set<string>
+  importMap: Map<string, string>
+  namespaces: Set<string>
+}
+
+const RUNTIME_IMPORT_MODULES = new Set([
+  RUNTIME_MODULE,
+  '@fictjs/runtime',
+  '@fictjs/runtime/advanced',
+  'fict',
+  'fict/advanced',
+])
+
+export function collectRuntimeImports(
+  body: BabelCore.types.Statement[],
+  t: typeof BabelCore.types,
+): RuntimeImportCollection {
+  const names = new Set<string>()
+  const importMap = new Map<string, string>()
+  const namespaces = new Set<string>()
+
+  for (const stmt of body) {
+    if (!t.isImportDeclaration(stmt)) continue
+    if ((stmt as { importKind?: string }).importKind === 'type') continue
+    if (!RUNTIME_IMPORT_MODULES.has(stmt.source.value)) continue
+    for (const spec of stmt.specifiers) {
+      if (t.isImportSpecifier(spec) && spec.importKind === 'type') {
+        continue
+      }
+      names.add(spec.local.name)
+      if (t.isImportSpecifier(spec)) {
+        const importedName = t.isIdentifier(spec.imported)
+          ? spec.imported.name
+          : spec.imported.value
+        importMap.set(spec.local.name, importedName)
+      } else if (t.isImportNamespaceSpecifier(spec) || t.isImportDefaultSpecifier(spec)) {
+        namespaces.add(spec.local.name)
+      }
+    }
+  }
+
+  return { names, importMap, namespaces }
+}
--- packages/compiler/src/ir/codegen.ts
+++ packages/compiler/src/ir/codegen.ts
@@ -2,7 +2,7 @@ import { pathToFileURL } from 'node:url'
 
 import type * as BabelCore from '@babel/core'
 
-import { DelegatedEvents, RUNTIME_ALIASES, RUNTIME_MODULE } from '../constants'
+import { DelegatedEvents, RUNTIME_ALIASES } from '../constants'
 import { debugEnabled, debugLog } from '../debug'
 import { applyRegionMetadata, shouldMemoizeRegion, type RegionMetadata } from '../fine-grained-dom'
 import { setModuleMetadata } from '../module-metadata'
@@ -30,6 +30,7 @@ import {
   getReactiveCallKindFromBabel,
   getStaticPropName,
 } from './codegen-reactive-kind'
+import { collectRuntimeImports } from './codegen-runtime-imports'
 import {
   HIRError,
   type BasicBlock,
@@ -2228,51 +2229,6 @@ function lowerTerminator(block: BasicBlock, ctx: CodegenContext): BabelCore.type
     default:
       return applyLoc([])
   }
-}
-
-interface RuntimeImportCollection {
-  names: Set<string>
-  importMap: Map<string, string>
-  namespaces: Set<string>
-}
-
-const RUNTIME_IMPORT_MODULES = new Set([
-  RUNTIME_MODULE,
-  '@fictjs/runtime',
-  '@fictjs/runtime/advanced',
-  'fict',
-  'fict/advanced',
-])
-
-function collectRuntimeImports(
-  body: BabelCore.types.Statement[],
-  t: typeof BabelCore.types,
-): RuntimeImportCollection {
-  const names = new Set<string>()
-  const importMap = new Map<string, string>()
-  const namespaces = new Set<string>()
-
-  for (const stmt of body) {
-    if (!t.isImportDeclaration(stmt)) continue
-    if ((stmt as { importKind?: string }).importKind === 'type') continue
-    if (!RUNTIME_IMPORT_MODULES.has(stmt.source.value)) continue
-    for (const spec of stmt.specifiers) {
-      if (t.isImportSpecifier(spec) && spec.importKind === 'type') {
-        continue
-      }
-      names.add(spec.local.name)
-      if (t.isImportSpecifier(spec)) {
-        const importedName = t.isIdentifier(spec.imported)
-          ? spec.imported.name
-          : spec.imported.value
-        importMap.set(spec.local.name, importedName)
-      } else if (t.isImportNamespaceSpecifier(spec) || t.isImportDefaultSpecifier(spec)) {
-        namespaces.add(spec.local.name)
-      }
-    }
-  }
-
-  return { names, importMap, namespaces }
 }
 
 function collectLocalDeclaredNames(
