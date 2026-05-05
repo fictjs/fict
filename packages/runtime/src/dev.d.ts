12f907319bd1cf8cb11d2317598aa9353cfe6683
unadlib <unadlib@gmail.com>
2026-05-06 04:38:28 +0800

    fix(others): align workspace typecheck config

--- packages/runtime/src/dev.d.ts
+++ packages/runtime/src/dev.d.ts
@@ -2,4 +2,12 @@ export {}
 
 declare global {
   const __DEV__: boolean | undefined
+  const process:
+    | {
+        env?: {
+          NODE_ENV?: string
+          [key: string]: string | undefined
+        }
+      }
+    | undefined
 }
